3e081135b22dae83d3f36cdadaff47e619797a64
sctigercat1 <sctigercat1@gmail.com>
2020-01-25 22:45:31 -0500

    Switch to simple DB

--- nutriview/nutriview/settings.py
+++ nutriview/nutriview/settings.py
@@ -81,24 +81,12 @@ WSGI_APPLICATION = 'nutriview.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': local_settings.DB_NAME or '',
-            'USER': local_settings.DB_USERNAME or '',
-            'PASSWORD': local_settings.DB_PASSWORD or '',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+}
 
 
 # Password validation
