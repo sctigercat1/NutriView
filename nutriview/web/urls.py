616047ceb992b35ddb926905649c356a659c9d99
sctigercat1 <sctigercat1@gmail.com>
2020-01-25 20:04:57 -0500

    Remove cv2, replace w/ html5 implementation

--- nutriview/web/urls.py
+++ nutriview/web/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.root),
-    path('feed', views.feed),
     path('analysis', views.analysis),
     path('snap', views.snap),
 	path('nutri', views.nutri),
